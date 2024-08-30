8cf1e30b467026da2b95578c5d4997d764e3f0c6
pawantanaytripathi <pawantanaytripathi@gmail.com>
2024-08-30 19:23:02 +0530

    upload destination rate sheet

--- functions/index.js
+++ functions/index.js
@@ -10,17 +10,51 @@ const puppeteer = require("puppeteer")
 const logger = require("firebase-functions/logger")
 const {onDocumentCreated} = require("firebase-functions/v2/firestore")
 const admin = require("firebase-admin")
+const functions = require('firebase-functions')
+const Busboy = require("busboy")
+const XLSX = require('xlsx')
+const express = require("express");
+const bodyParser = require("body-parser")
+const { getAuth } = require("firebase-admin/auth")
 
 admin.initializeApp()
 const bucket = admin.storage().bucket()
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+const auth = getAuth()
 
 const configMap = {
     baseViewPackageUrl: 'https://stayeasy-7ac2c.web.app/package/{packageCode}/pdf',
     networkPageWaitUntil: 'networkidle2',
     basePdfFileName: 'pkg-{packageCode}.pdf',
-    bucketDirName: 'package-pdfs',
-    bucketPublicBaseUrl: 'https://storage.googleapis.com/{bucketName}/package-pdfs/{pdfFileName}'
+    pkgPdfBucketDirName: 'package-pdfs',
+    destinationRateSheetBucketDirName: 'ta-destination-rate-sheets',
+    pkgPdfBucketPublicBaseUrl: 'https://storage.googleapis.com/{bucketName}/package-pdfs/{pdfFileName}',
+    destinationRateSheetBucketPublicBaseUrl: 'https://storage.googleapis.com/{bucketName}/rate-sheets/{excelFileName}'
 }
+
+app.use(async (req, res, next) => {
+    let idToken = req.headers.authorization;
+    if (!idToken) {
+        res.status(400).send("authorization header missing.")
+        return;
+    }
+    try {
+        let decodedToken = await auth.verifyIdToken(idToken)
+        const uid = decodedToken.uid
+        res.locals.user = decodedToken.phone_number
+        res.locals.authenticated = true
+        next()
+    } catch (e) {
+        res.status(400).send("Auth token invalid");
+        return false;
+    }
+})
+
+exports.webApi = functions.https.onRequest(app)
 
 exports.generatePackagePdf = onDocumentCreated(
     "/packages/{packId}",
@@ -85,20 +119,73 @@ exports.generatePackagePdf = onDocumentCreated(
                             // todo: take care of file security rules for files in firebase bucket
 
                             return bucket.upload(pdfFileName, {
-                                destination: `${configMap.bucketDirName}/${pdfFileName}`,
+                                destination: `${configMap.pkgPdfBucketDirName}/${pdfFileName}`,
                             }).then(async () => {
-                                const publicUrl = configMap.bucketPublicBaseUrl.replace('{bucketName}',
+                                const publicDownloadUrl = configMap.pkgPdfBucketPublicBaseUrl.replace('{bucketName}',
                                     bucket.name).replace('{pdfFileName}', pdfFileName)
 
                                 // example url -> https://storage.googleapis.com/stayeasy-7ac2c.appspot.com/package-pdfs/pkg-lz0hzMb3a3wAme4vQlEH.pdf
 
-                                logger.log("File Download Link:", publicUrl);
+                                logger.log("File Download Link:", publicDownloadUrl)
+
+                                return event.data.ref.update({
+                                    pdfDownloadUrl: publicDownloadUrl
+                                }).then(async () => true)
                             })
                         })
                     })
                 })
             })
+        }).catch((err) => {
+            logger.log("Package PDF creation failed: ", err)
         })
 
     }
 )
+
+app.post("/users/:userId/destinations/:destinationName/upload-rate-sheet/", async (req, res) => {
+    try{
+        const busboy = Busboy({headers: req.headers});
+        let fileBuffer = Buffer.alloc(0);
+        let fileName;
+
+        // Listen for 'file' event to capture the uploaded file data
+        busboy.on('file', (fieldName, file, fileDetails) => {
+            fileName = fileDetails.filename
+            logger.log(`Received rate sheet: ${fileName}`);
+
+            // Accumulate the file data in memory
+            file.on('data', (data) => {
+                fileBuffer = Buffer.concat([fileBuffer, data]);
+            });
+
+            file.on('end', () => {
+                logger.log(`Received TA Destination Rate sheet: ${fileDetails.filename}`)
+
+                const workbook = XLSX.read(fileBuffer, {type: 'buffer'});
+                // Convert the first sheet to JSON
+                const sheetName = workbook.SheetNames[0];
+                const worksheet = workbook.Sheets[sheetName];
+                const jsonData = XLSX.utils.sheet_to_json(worksheet);
+            })
+        })
+
+        busboy.on('error', (error) => {
+            return res.status(500).send(`Error while processing file: ${error}`);
+        })
+
+        busboy.on('finish', () => {
+            return res.status(201).send({message: 'Rate sheet uploaded successfully'})
+        })
+
+        if (req.rawBody) {
+            busboy.end(req.rawBody);
+        } else {
+            req.pipe(busboy);
+        }
+    }
+    catch(err){
+        logger.log(`Uploaded rate sheet processing failed => ${err}`)
+        return res.status(500).send({message: 'Something went wrong processing your request'})
+    }
+})
