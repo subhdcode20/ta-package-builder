69a48a3f6535af1d66085cae45039bbefe678db6
Subham Dey <subhdcode@gmail.com>
2024-10-20 00:19:38 +0530

    small pdf ui changes

--- src/Ratesheet/viewActiveRatesheets.js
+++ src/Ratesheet/viewActiveRatesheets.js
@@ -20,10 +20,10 @@ import { Grid } from '@mui/material';
 
 const RateSheetTable = ({ reqsList = [] }) => {
 	const [destination, setDestination] = useState("");
-    const [userRates, setUserRates] = React.useState({});
-    const {userData} = useContext(MainContext);
-    const [expanded, setExpanded] = React.useState(false);
-    const navigate = useNavigate();
+  const [userRates, setUserRates] = React.useState({});
+  const {userData} = useContext(MainContext);
+  const [expanded, setExpanded] = React.useState(false);
+  const navigate = useNavigate();
 	const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
 
     const handleChange = (panel) => (event, isExpanded) => {
@@ -159,75 +159,108 @@ const RateSheetTable = ({ reqsList = [] }) => {
   console.log("req list table", userData, destination, userRates, userRates[destination.toLowerCase()]);
   const tableData = userRates[destination.toLowerCase()]?.hotels  || [];
   return (<>
-    <Box sx={{ margin: "auto", width: "100%", my: 2 }}>
-        <MainDestSelect handleDestSelect={handleDestSelect} destination={destination} />
-    </Box>
-
-        
-    <Typography variant="subtitle1" sx={{ my: 1 }}>
-        Active Ratesheets for <b>{`${destination}`}</b>:
-    </Typography>
-    <Box sx={{ margin: "auto", height: 400, width: "100%" }}>
-        {
-            tableData.map((rate) => <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1bh-content"
-                id="panel1bh-header"
-            >
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                {rate.hotelName}
+    <hr style={{ margin: 'auto', maxWidth: '50%', marginTop: '16px', marginBottom: '16px' }} />
+    <Box sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        // minHeight: "90vh",
+        padding: 2,
+        borderRadius: 4,
+        border: "1px solid #ccc",
+        bgcolor: "transparent",
+        paddingX: 3,
+        alignItems: 'center'
+    }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', mb: 3 }}>
+            <Typography variant="h6" sx={{ fontWeight: 600, fontSize: '20px', marginTop:1 }}>
+                Upload Ratesheet
             </Typography>
-            <Typography sx={{ color: 'text.secondary' }}>{format(rate?.validFrom, "dd-MMM-yyyy")} - {format(rate?.validUntil, "dd-MMM-yyyy")}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Grid container display={'flex'}>
+        </Box>
+        {/* <Box sx={{ display: 'flex', flexDirection: 'row', margin: "auto", width: "100%"}}>
+          <Typography variant="body1" sx={{ mt: 2 }}>
+              Check your Rate sheet Data:
+          </Typography>
+          &nbsp;
+        </Box> */}
+        
+        <Box flex={1} sx={{ ml: 2, width: isMobile ? '100%' : '55%' }}>
+          <MainDestSelect handleDestSelect={handleDestSelect} destination={destination} />
+        </Box>
+        {
+          (tableData && tableData.length > 0) &&  <Typography variant="subtitle1" sx={{ my: 1 }}>
+            Active Ratesheets for <b>{`${destination}`}</b>:
+          </Typography>
+        }
+        {
+          (destination && tableData.length == 0) && (
+            <Typography variant="subtitle1" sx={{ my: 3, mx: 'auto' }}>
+              No Rate sheet uploaded for <b>{destination} !</b>
+            </Typography>
+          )
+        }
+        <Box sx={{ margin: "auto", height: 400, width: "100%" }}>
+            {
+              tableData.map((rate) => <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                    >
+                <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                    {rate.hotelName}
+                </Typography>
+                <Typography sx={{ color: 'text.secondary' }}>Validity: {format(rate?.validFrom, "dd-MMM-yyyy")} - {format(rate?.validUntil, "dd-MMM-yyyy")}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Grid container display={'flex'}>
 
-                    {
-                        Object.keys(rate.roomRates).map((room) => (<>
-                            <Grid item xs={12} md={12} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
-                                <hr style={{ width: '100%' }} />
-                            </Grid>
-                            <Grid item xs={12} md={3} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
-                                <Typography>Room Name</Typography>
-                                &nbsp;
-                                <Typography><b>{room}</b></Typography>
-                            </Grid>
-                            <Grid item xs={12} md={3} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
-                                <Typography>Cpai Price</Typography>
-                                &nbsp;
-                                <Typography><b>Rs. {rate.roomRates[room]?.cpaiPrice}</b></Typography>
-                            </Grid>
-                            <Grid item xs={12} md={3} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
-                                <Typography>Mapai Price</Typography>
-                                &nbsp;
-                                <Typography><b>Rs. {rate.roomRates[room]?.mapaiPrice}</b></Typography>
-                            </Grid>
-                            <Grid item xs={12} md={3} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
-                                <Typography>Apai Price</Typography>
-                                &nbsp;
-                                <Typography><b>Rs. {rate.roomRates[room]?.apaiPrice}</b></Typography>
-                            </Grid>
-                        </>))
-                    }
-                    
-                </Grid>
-            </AccordionDetails>
-        </Accordion>)}
-      {/* <DataGrid
-        rows={tableData}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
-            },
-          },
-        }}
-        pageSizeOptions={[7]}
-        checkboxSelection={false}
-        disableRowSelectionOnClick
-      /> */}
+                        {
+                          Object.keys(rate.roomRates).map((room) => (<>
+                                <Grid item xs={12} md={12} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
+                                    <hr style={{ width: '100%' }} />
+                                </Grid>
+                                <Grid item xs={12} md={3} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
+                                    <Typography>Room Name</Typography>
+                                    &nbsp;
+                                    <Typography><b>{room}</b></Typography>
+                                </Grid>
+                                <Grid item xs={12} md={3} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
+                                    <Typography>Cpai Price</Typography>
+                                    &nbsp;
+                                    <Typography><b>Rs. {rate.roomRates[room]?.cpaiPrice}</b></Typography>
+                                </Grid>
+                                <Grid item xs={12} md={3} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
+                                    <Typography>Mapai Price</Typography>
+                                    &nbsp;
+                                    <Typography><b>Rs. {rate.roomRates[room]?.mapaiPrice}</b></Typography>
+                                </Grid>
+                                <Grid item xs={12} md={3} display={'flex'} flexDirection={isMobile ? 'row' : 'column'} justifyContent={isMobile ? 'space-between' : 'flex-start'} width={'inherit'}>
+                                    <Typography>Apai Price</Typography>
+                                    &nbsp;
+                                    <Typography><b>Rs. {rate.roomRates[room]?.apaiPrice}</b></Typography>
+                                </Grid>
+                            </>))
+                        }
+                        
+                    </Grid>
+                </AccordionDetails>
+            </Accordion>)}
+          {/* <DataGrid
+            rows={tableData}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 5,
+                  },
+                  },
+                  }}
+                  pageSizeOptions={[7]}
+                  checkboxSelection={false}
+                  disableRowSelectionOnClick
+                  /> */}
+        </Box>
     </Box>
   </>);
 }
