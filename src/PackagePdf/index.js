e982f2f8c2f53caf8d3b94e37889fbed46272873
Subham Dey <subhdcode@gmail.com>
2024-11-21 02:15:17 +0530

    base 64 api integration

--- src/PackagePdf/index.js
+++ src/PackagePdf/index.js
@@ -5,6 +5,7 @@ import { doc, getDoc } from 'firebase/firestore';
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Box from "@mui/material/Box";
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 import { db } from '../firebaseConfig';
 import { MainContext } from '../Utility.js';
@@ -22,22 +23,55 @@ const PackagePdf = ({ pkgSelectedHotels = [], reqData = {} 	 , totalPrice=null})
 	// const totalDayPrices = useSelector((state) => state.packBuilderData.totalDayPrices) || [];
 	const finalPackPrice = useSelector((state) => state.packBuilderData.finalPackPrice) || [];
 	const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
+	let { companyInfo: { companyLogo: logoUrl } = {}, firebaseIdToken = '' } = userData;
 
-    useEffect(() => {
-		const getUserPdfData = async () => {
-			let docSnapPdfData = await getDoc(doc(db, "userPdfData", userData.phone));
-			if (docSnapPdfData.exists()) {
-				console.log("DocSnapPdfData", docSnapPdfData.data());
-				let { logoB64Str = null } = docSnapPdfData.data();	
-				setUserPdfData({ 
-					...userData, 
-					logoB64Str
-				})
-			}
+	const getLogoB64encoded = async () => {
+		const axiosOptions = {
+		  method: 'POST',
+		  headers: { 
+			  // 'content-type': 'application/x-www-form-urlencoded',
+			  'Authorization': firebaseIdToken 
+		  },
+		  data: {
+			logoUrl: logoUrl
+		  },
+		  url: `${process.env.REACT_APP_API_DOMAIN}/api/get-logo-b64`,
+		};
+		console.log('request axios ', axiosOptions);
+	
+		let response = await axios(axiosOptions);
+		console.log('response axios ', response);
+		setUserPdfData(prev => ({ 
+			...prev, 
+			logoB64Str: response.data?.data
+		}))
+		// setNewUserData(prev => ({ ...prev, logoB64Str: response.data?.data }));
+	}
+
+    // useEffect(() => {
+	// 	// const getUserPdfData = async () => {
+	// 	// 	let docSnapPdfData = await getDoc(doc(db, "userPdfData", userData.phone));
+	// 	// 	if (docSnapPdfData.exists()) {
+	// 	// 		console.log("DocSnapPdfData", docSnapPdfData.data());
+	// 	// 		let { logoB64Str = null } = docSnapPdfData.data();	
+	// 	// 		setUserPdfData({ 
+	// 	// 			...userData, 
+	// 	// 			logoB64Str
+	// 	// 		})
+	// 	// 	}
+	// 	// }
+
+	// 	// getUserPdfData();
+	// 	// getLogoB64encoded();
+	// }, [userData])
+
+	useEffect(() => {
+		console.log("CHECKBIDDATAINSIDE axios LOGO - ", logoUrl)
+		if(logoUrl) {
+		  //fetch logo base64
+		  getLogoB64encoded();
 		}
-
-		getUserPdfData();
-	}, [userData])
+	}, [logoUrl]);
 
     if (loading) {
         return <Typography>Loading...</Typography>;
